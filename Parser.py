c615d301d367dafbe6ba20c071675ab04cdbad20
zachary123212 <zlindenbaum@gmail.com>
2016-07-07 16:17:20 -0400

    implemented parsing for paragraphs and quotes

--- Parser.py
+++ Parser.py
@@ -1,10 +1,47 @@
-from pyparsing import *
+from pyparsing import (Suppress, SkipTo, Literal, OneOrMore, Word,
+                       alphanums, StringEnd)
 
-italic = Group(Suppress(Literal("*")) + SkipTo(Literal("*")) + Suppress(Literal("*")))
-bold = Group(Suppress(Literal("**")) + SkipTo(Literal("**")) + Suppress(Literal("**")))
+# Ex: *this* is italic
 
-text = OneOrMore(bold | italic)
+italic_text = (
+    Suppress(Literal("*")) +
+    SkipTo(Literal("*")) +
+    Suppress(Literal("*"))
+).setParseAction(lambda t: [["italic", t[0]]])
 
-print(text.parseString("**this is an bold expression** *this is an italic one*"))
+# Ex: **this** is bold
 
+bold_text = (
+    Suppress(Literal("**")) +
+    SkipTo(Literal("**")) +
+    Suppress(Literal("**"))
+).setParseAction(lambda t: [["bold", t[0]]])
 
+# Ex: this is regular
+
+reg_text = (
+    OneOrMore(Word(alphanums))
+).setParseAction(lambda t: [["regular", " ".join(t)]])
+
+text = OneOrMore(bold_text | italic_text | reg_text)
+
+# Note: can be either two newlines or the end of the string
+
+line_break = Suppress(Literal("\\n\\n") | StringEnd())
+
+paragraph = (
+    OneOrMore(text) +
+    line_break
+).setParseAction(lambda t: [["paragraph", t.asList()]])
+
+quote = (
+    Suppress(Literal(">")) +
+    text +
+    line_break
+).setParseAction(lambda t: [["quote", t.asList()]])
+
+markdown = OneOrMore(quote | paragraph)
+
+input_string = "Lorem ipsum *dolor* sit\\n\\namet and you know **it**\\n\\n > *the* person said this\\n\\n`"
+
+print(markdown.parseString(input_string))
