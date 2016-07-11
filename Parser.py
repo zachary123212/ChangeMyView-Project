2da955defebaeb6c830e1b40744d2be4b79edc90
zachary123212 <zlindenbaum@gmail.com>
2016-07-11 14:30:14 -0400

    got parser to ignore single newlines and allow more than 3 to indicate new paragraph

--- Parser.py
+++ Parser.py
@@ -1,5 +1,5 @@
 from pyparsing import (Suppress, SkipTo, Literal, OneOrMore, Word, ParserElement,
-                       alphanums, StringEnd, printables)
+                       alphanums, StringEnd, printables, Optional, ZeroOrMore)
 
 # Ex: *this* is italic
 
@@ -17,17 +17,30 @@ bold_text = (
     Suppress(Literal("**"))
 ).setParseAction(lambda t: [["bold", t[0]]])
 
+bold_italic_text = (
+    Suppress(Literal("***")) +
+    SkipTo(Literal("***")) +
+    Suppress(Literal("***"))
+).setParseAction(lambda t: [["bold-italic", t[0]]])
+
 # Ex: this is regular
 
 reg_text = (
-    OneOrMore(Word(printables.replace("*","")))
+    OneOrMore(Word(printables.replace("*","")) | Suppress(Literal("\n")) + Word(printables))
+    # SkipTo(Literal("*")) | SkipTo(Literal("\n\n"))
 ).setParseAction(lambda t: [["regular", " ".join(t)]])
 
-text = OneOrMore(bold_text | italic_text | reg_text)
+text = OneOrMore(bold_italic_text| bold_text | italic_text | reg_text)
 
 # Note: can be either two newlines or the end of the string
 
-line_break = Suppress((Literal("\n\n")) | StringEnd())
+line_break = Suppress(
+    Literal("\n\n") + ZeroOrMore(Literal("\n")) |
+    Literal("\n") + StringEnd() |
+    StringEnd()
+)
+
+divider = Suppress(OneOrMore(Literal("_")))
 
 paragraph = (
     OneOrMore(text) +
@@ -40,7 +53,10 @@ quote = (
     line_break
 ).setParseAction(lambda t: [["quote", t.asList()]])
 
-markdown = OneOrMore(quote | paragraph)
+markdown = (
+    Suppress(Optional(OneOrMore(Literal("\n")))) +
+    OneOrMore(quote | paragraph | divider)
+)
 
 ParserElement.setDefaultWhitespaceChars(' \t')
 
--- Read.py
+++ Read.py
@@ -15,9 +15,47 @@ with open("train_pair_data.jsonlist", "r") as raw:
 def process_text(input_text):
     return input_text.replace("&gt;", ">")
 
-parsed = markdown.parseString(process_text(
-    data[3]['positive']['comments'][1]['body'])).asList()
+def parse(input_text):
+    return markdown.parseString(process_text(input_text)).asList()
 
-pp = pprint.PrettyPrinter(indent=4,width=100,depth=6)
+# data_p = []
+#
+# for thread in data:
+#     info = {}
+#     info['op_author'] = thread['op_author']
+#     print(thread['op_text'].encode('utf8'))
+#     info['op_text'] = parse(thread['op_text'])
+#     info['positive'] = []
+#     info['negative'] = []
+#
+#     for comment in thread['positive']['comments']:
+#         infoC = {}
+#         infoC['author'] = comment['author']
+#         infoC['text'] = parse(comment['body'])
+#         info['positive'].append(infoC)
+#     for comment in thread['negative']['comments']:
+#         infoC = {}
+#         infoC['author'] = comment['author']
+#         infoC['text'] = parse(comment['body'])
+#         info['positive'].append(infoC)
+#
+# parsed = markdown.parseString(process_text(
+#     data[3]['positive']['comments'][1]['body'])).asList()
+#
+# pp = pprint.PrettyPrinter(indent=4,width=100,depth=6)
+#
+# pp.pprint(parsed)
 
-pp.pprint(parsed)
+test = """
+I can't remember **the topic that** spurred this
+discussion,
+but a friend and I were debating whether man-made things were natural. He took the position that they are unnatural.
+
+
+
+He cited this definition by Merriam-Webster:  existing in nature and not made or caused by people : coming from nature (http://www.merriam-webster.com/dictionary/natural) as his basis for the distinction for natural vs. unnatural.
+
+However, I respectfully disagree with his position and furthermore that definition of natural. People arise from nature. Humankind's capacity to create, problem-solve, analyze, rationalize, and build also come from natural processes. How are the things we create unnatural? It is only through natural occurrences that we have this ability, why is it that we would give the credit of these things solely to man, as opposed to nature? We are not separate from nature, thus, how can any of our actions or creations be unnatural? If we were somehow separate from nature, I would understand the distinction between natural and man-made. However, I think unnatural and man-made are not synonyms by any means. It seems to me that man-made things MUST be natural due to our being part of nature.
+"""
+
+print(parse(test))
