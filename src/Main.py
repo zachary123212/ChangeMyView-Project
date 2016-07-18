3dd2092276465dff7fd37ff4e4611450424cfe00
zachary123212 <zlindenbaum@gmail.com>
2016-07-18 14:47:54 -0400

    organized Main.py with comments
    Former-commit-id: 125a67b7837db1c8e3bbddc87d82ffc9f638ccba

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import operator
 import os
 import pickle
 import pprint
+import csv
 
 import nltk
 from nltk import RegexpTokenizer
@@ -23,6 +24,8 @@ CONCESSIONS = ["nevertheless", "nonetheless", "non the less", "however", "admitt
 # Main Procedure:
 
 def main():
+    # Serialized Data Reading/Writing
+
     with open("data/data.pickle", 'r+b') as raw:
         if os.stat("data/data.pickle").st_size == 0:
             pickle.dump(read("data/train_pair_data.jsonlist"), raw)
@@ -30,22 +33,18 @@ def main():
         data_p = pickle.load(raw)
         print("data loaded")
 
+    # Text Extraction
+
     texts_p = [[comment['text_plain'] for comment in thread['positive']] for thread in data_p]
     texts_p = [t[0] for t in texts_p if t != []]
 
     texts_n = [[comment['text_plain'] for comment in thread['negative']] for thread in data_p]
     texts_n = [t[0] for t in texts_n if t != []]
 
+    # Concession Calculation
+
     concession_frequencies_p = {}
-    word_count_p = 0
-
     concession_frequencies_n = {}
-    word_count_n = 0
-
-    for text in texts_p:
-        word_count_p += len(tokenizer.tokenize(text))
-    for text in texts_n:
-        word_count_n += len(tokenizer.tokenize(text))
 
     for concession in CONCESSIONS:
         concession_frequencies_p[concession] = []
@@ -57,19 +56,14 @@ def main():
         for text in texts_p:
             if concession in text:
                 concession_frequencies_p[concession][0] += 1
-
-                # concession_frequencies_p[concession][0] += text.count(concession)
         for text in texts_n:
             if concession in text:
                 concession_frequencies_n[concession][0] += 1
 
-                # concession_frequencies_n[concession][0] += text.count(concession)
-
         concession_frequencies_p[concession].append(concession_frequencies_p[concession][0] / len(texts_p))
         concession_frequencies_n[concession].append(concession_frequencies_n[concession][0] / len(texts_n))
 
-        # concession_frequencies_p[concession].append(concession_frequencies_p[concession][0] / word_count_p)
-        # concession_frequencies_n[concession].append(concession_frequencies_n[concession][0] / word_count_n)
+    # Print Output
 
     with open("data/results.txt", "w") as raw:
         print("positive:\n")
