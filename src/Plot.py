730ac41f1ea6f44a2786b68ed039f7afb0af526f
zachary123212 <zlindenbaum@gmail.com>
2016-07-26 15:12:56 -0400

    got multi-word concessions to work in hist

--- src/Plot.py
+++ src/Plot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
+from nltk import word_tokenize
 
 
 def calc(data_p, input_word, register):
     freqs = []
+    word_length = len(word_tokenize(input_word))
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -10,9 +12,11 @@ def calc(data_p, input_word, register):
     for thread in data_p:
         for comment in thread[register]:
             for sentence_i in range(0, len(comment['text_tokenized'])):
-                for word in comment['text_tokenized'][sentence_i]:
-                    if word.lower() == input_word:
-                        freqs.append(sentence_i / len(comment['text_tokenized']))
-    ax.hist(freqs, 20, color='green', alpha=0.8)
+                for word_i in range(0, len(comment['text_tokenized'][sentence_i])):
+                    if input_word == " ".join(
+                            [word for word in comment['text_tokenized'][sentence_i][word_i:(word_i + word_length)]]):
+                        freqs.append((sentence_i + 1)/len(comment['text_tokenized']))
+
+    ax.hist(freqs, 5, color='green', alpha=0.8)
     plt.savefig("data/hist/" + input_word.replace(" ", "_") + "_" + register + ".png")
     plt.close()
